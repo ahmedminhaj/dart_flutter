27e07b022a732ec264700095b2aa96f30ed1176e
ahmedminhaj <ahmedminhaj.pro@gmail.com>
2019-11-14 19:32:55 +0600

    dart - class

--- dart_class/bin/main.dart
+++ dart_class/bin/main.dart
@@ -1,21 +1,30 @@
+class Student{
+  String name;
+  int age;
+  double num1, num2;
 
-void printList(var l){
-  for(int i=0; i<l.length; i++ ){
-    print(l[i]);
+  Student(String name, int age, double num1, double num2){
+    this.name = name;
+    this.age = age;
+    this.num1 = num1;
+    this.num2 = num2;
   }
 }
 
-main(List<String> arguments) {
+double averageMark(double num1, double num2){
+  double t = num1+num2;
+  return (t/2);
+}
 
-  var person = "jack";
-  print('Hello $person');
-  var list = [1, 2, 5, 7];
-  printList(list);
-  var names = ['rock', 'rob', 'bob'];
-  printList(names);
+void main() {
+  var s1 = Student('Rob', 17, 78, 87);
+  var s2 = Student('Bob', 16, 81, 88);
 
-  var emo = '\u{1f44f}';
-  print(emo);
-  var emo1 = '\u{1f44d}';
-  print(emo1);
+  print(s1.name);
+  print(s1.age);
+  print(averageMark(s1.num1,s1.num2));
+  print(s2.name);
+  print(s2.age);
+  print(averageMark(s2.num1,s2.num2));
+
 }
