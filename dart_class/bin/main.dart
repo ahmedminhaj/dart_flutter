c225cc0da065390cfca44346f9ca96931455452c
ahmedminhaj <ahmedminhaj.pro@gmail.com>
2019-11-16 19:25:22 +0600

    class constructor, dart list map

--- dart_class/bin/main.dart
+++ dart_class/bin/main.dart
@@ -9,6 +9,10 @@ class Student{
     this.num1 = num1;
     this.num2 = num2;
   }
+
+  Student.university(this.name, this.num1, this.num2){
+    age=20;
+  }
 }
 
 double averageMark(double num1, double num2){
@@ -19,12 +23,10 @@ double averageMark(double num1, double num2){
 void main() {
   var s1 = Student('Rob', 17, 78, 87);
   var s2 = Student('Bob', 16, 81, 88);
+  var s3 = Student.university('Jacob', 77, 75);
 
-  print(s1.name);
-  print(s1.age);
-  print(averageMark(s1.num1,s1.num2));
-  print(s2.name);
-  print(s2.age);
-  print(averageMark(s2.num1,s2.num2));
+  print('Name:'+ s1.name+ ', Age:' + (s1.age).toString()+ ', Avarage Mark:' + (averageMark(s1.num1,s1.num2)).toString());
+  print('Name:'+ s2.name+ ', Age:' + (s2.age).toString()+ ', Avarage Mark:' + (averageMark(s2.num1,s2.num2)).toString());
+  print('Name:'+ s3.name+ ', Age:' + (s3.age).toString()+ ', Avarage Mark:' + (averageMark(s3.num1,s3.num2)).toString());
 
 }
